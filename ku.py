9b149f934b4114f6b53c25cba41a08899ebeaa9d
Amrutha Varshini Immidisetti <23P31A4229@acet.ac.in>
2026-03-18 11:38:00 +0530

    Update ku.py

--- ku.py
+++ ku.py
@@ -3,15 +3,12 @@ import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
 
-# Load data
 num_words = 20000
 (x_train, y_train), (x_test, y_test) = keras.datasets.imdb.load_data(num_words=num_words)
 
-# Pad sequences
 x_train = keras.preprocessing.sequence.pad_sequences(x_train, maxlen=256)
 x_test = keras.preprocessing.sequence.pad_sequences(x_test, maxlen=256)
 
-# Build model
 model = keras.Sequential([
     keras.layers.Embedding(num_words, 2, input_length=256),
     keras.layers.Flatten(),
@@ -20,17 +17,13 @@ model = keras.Sequential([
     keras.layers.Dense(1, activation='sigmoid')
 ])
 
-# Summary
 model.summary()
 
-# Compile
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Train
 history = model.fit(x_train, y_train, epochs=5, batch_size=100,
                     validation_data=(x_test, y_test), verbose=1)
 
-# Plot accuracy
 plt.plot(history.history['accuracy'], label='Train')
 plt.plot(history.history['val_accuracy'], label='Validation')
 plt.title("Accuracy")
