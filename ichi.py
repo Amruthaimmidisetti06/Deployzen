28a5edfb9e6dcba8e1dd3a9b31e8b55fb0a8d358
Amrutha Varshini Immidisetti <23P31A4229@acet.ac.in>
2026-03-18 11:37:18 +0530

    Update ichi.py

--- ichi.py
+++ ichi.py
@@ -2,17 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import mnist
 
-# Load data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Print label counts
 print("Train:", dict(zip(*np.unique(y_train, return_counts=True))))
 print("\nTest:", dict(zip(*np.unique(y_test, return_counts=True))))
 
-# Select 25 random images
 idx = np.random.randint(0, len(x_train), 25)
 
-# Plot images
 plt.figure(figsize=(5,5))
 for i, j in enumerate(idx):
     plt.subplot(5,5,i+1)
